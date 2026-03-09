3de1d8c3c47523127cf3a9cb8b12fdb93d0d0899
leesungwook <lsuk3165@pukwan.co.kr>
2026-03-09 15:15:05 +0900

    강지해변 ->감지

--- project/dashboard/map/layers/layers.js
+++ project/dashboard/map/layers/layers.js
@@ -115,7 +115,7 @@ var lyr____6 = new ol.layer.Vector({
                 interactive: true,
     title: '영도_중리산권_관광벨트<br />\
     <img src="styles/legend/___6_0.png" /> <br />\
-    <img src="styles/legend/___6_1.png" /> (강지해변 배후지)<br />\
+    <img src="styles/legend/___6_1.png" /> (감지해변 배후지)<br />\
     <img src="styles/legend/___6_2.png" /> (예비군 훈련장 이전적지)<br />\
     <img src="styles/legend/___6_3.png" /> (중리해변 배후지)<br />' });
 var format____7 = new ol.format.GeoJSON();
